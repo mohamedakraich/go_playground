3e2de654551af89495c42ff7b904748d330442e9
goenix <mohamed.akraich@gmail.com>
2023-02-03 13:16:36 +0100

    ipgetheadinfo added

---
+++ networking/14_ipgetheadinfo/ipgetheadinfo.go
@@ -0,0 +1,51 @@
+/* IPGetHeadInfo
+ */
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+// go run ipgetheadinfo.go yahoo.com:80 # can't use go.dev as its on 443 w/TLS
+func main() {
+	if len(os.Args) != 2 {
+		log.Printf("Usage: %s host:port", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+	result, err := readFully(conn)
+	checkError(err)
+	fmt.Println(string(result))
+}
+func readFully(conn net.Conn) ([]byte, error) {
+	defer conn.Close()
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+	return result.Bytes(), nil
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Printf("Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
