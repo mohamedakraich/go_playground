85fa8116fa812a4111d48c92edaf2d2873ae3e5a
goenix <mohamed.akraich@gmail.com>
2023-03-11 13:14:31 +0100

    add mechanics of panic and defer

--- defer/main.go
+++ defer/main.go
@@ -33,4 +33,30 @@ func main() {
 	b()
 	c()
 	fmt.Println("https://go.dev/blog/defer-panic-and-recover")
+	fmt.Println("mechanics of panic and defer")
+	f()
+	fmt.Println("Returned normally from f.")
 }
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
+
+// https://pkg.go.dev/encoding/json
